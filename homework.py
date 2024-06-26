2f3ff9fc1eaf9b7ae70571bd02f88dfa74ec6ce0
蔡孟儒 <ppksgopin@gmail.com>
2024-06-26 14:04:16 +0800

    homework ver1

--- homework.py
+++ homework.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, Path
 from pydantic import BaseModel, Field
 from starlette import status
+import re
 
 app = FastAPI()
 
@@ -30,6 +31,21 @@ user = {
 }
 
 
+def checkMail(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    if(re.fullmatch(regex, email)):
+    	return True
+    else:
+        return False
+
+def checkDuplicate(inputUser):
+
+	
+
+@app.get("/getAll")
+async def getAllUser():
+    return user
+
 @app.get("/getuser/{user_id}", status_code = status.HTTP_200_OK)
 async def inspect_user_by_id(user_id: int = Path(gt = 0)):
     info_get = False
@@ -45,22 +61,26 @@ async def inspect_user_by_id(user_id: int = Path(gt = 0)):
 
 @app.post("/createuser", status_code = status.HTTP_201_CREATED)
 async def create_user(user_request: UserRequest):
+    new_user = Users(**user_request.model_dump())
     create_flag = False
-    try:
-        new_user = Users(**user_request.model_dump())
+
+    msg = {}
+    mailFlag = checkMail(new_user.email)
+    
+    if mailFlag:
         user[list(user)[-1] + 1] = new_user
         create_flag = True
-    except:
-        raise HTTPException(status_code=500, detail='Add new user failed.')
-
-    if create_flag:
+        msg = {"msg": "Add new user succeed.", "user_id": list(user)[-1]}
+        return msg
+    else:
         raise  HTTPException(status_code = 400, detail='Wrong input parameters.')
-
-
 
 @app.put("/updateuser/{user_id}", status_code = status.HTTP_200_OK)
 async def update_user(update_user: UserRequest, user_id: int):
-    if user_id <= 0:
+    msg = {}
+    mailFlag = checkMail(update_user.email)
+
+    if (user_id <= 0) or (not mailFlag):
         raise HTTPException(status_code = 400, detail = 'Wrong input parameters.')
     else:
         info_changed = False
@@ -71,15 +91,23 @@ async def update_user(update_user: UserRequest, user_id: int):
                 break
         if not info_changed:
             raise HTTPException(status_code = 404, detail = 'User not exist.')
+        else:
+            msg = {"msg": "Update user " + str(user_id) + " data succeed."}
+            return msg
 
 
 @app.delete("/deluser/{user_id}", status_code = status.HTTP_200_OK)
 async def delete_user(user_id: int = Path(gt = 0) ):
     user_deleted = False
+    msg = {}
     for i in user:
         if i == user_id:
             del user[user_id]
             user_deleted = True
             break
+            
     if not user_deleted:
         raise HTTPException(status_code = 404, detail = 'User not exist.')
+    else:
+        msg = {"msg": "Delete user " + str(user_id) + " succeed."}
+        return msg
