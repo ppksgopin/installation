54a73b233e6b3f4dbc1c89edcff41d8a5467006e
蔡孟儒 <ppksgopin@gmail.com>
2024-07-13 19:08:32 +0800

    Update dockerInstallation.sh

--- dockerInstallation.sh
+++ dockerInstallation.sh
@@ -3,6 +3,8 @@ dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.r
 
 dnf install pass -y
 
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker
+
 dnf install docker-ce docker-ce-cli containerd.io -y
 
 systemctl start docker && systemctl enable --now docker
