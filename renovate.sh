54a3716b53e8924e96d20ddec3e03132e5c9daa0
蔡孟儒 <ppksgopin@gmail.com>
2024-07-14 19:53:20 +0800

    Update renovate.sh

--- renovate.sh
+++ renovate.sh
@@ -3,11 +3,13 @@
 sudo dnf install {kernel-headers,kernel-devel} -y
 sudo dnf update -y
 
-sudo dnf install ntfs-3g -y
 
 sudo dnf install epel-release -y
 sudo dnf install https://download1.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm -y
 sudo yum install vlc -y
+
+sudo dnf install ntfs-3g -y
+
 
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
 sudo dnf install pass -y
