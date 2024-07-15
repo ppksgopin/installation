3a5a96a05178142a5ede4312e1ca9743395221b8
蔡孟儒 <ppksgopin@gmail.com>
2024-07-15 11:37:14 +0800

    Update renovate.sh

--- renovate.sh
+++ renovate.sh
@@ -13,3 +13,12 @@ dnf install ntfs-3g -y
 
 sudo dnf install https://download1.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm -y
 sudo yum install vlc -y
+
+dnf install {virt-install,virt-viewer,libvirt,virt-manager,virt-top,libguestfs-tools} -y
+systemctl start libvirtd && systemctl enable --now libvirtd
+
+nmcli conn add type bridge con-name kvmbr0 ifname kvmbr0
+nmcli conn modify kvmbr0 ipv4.address 192.168.1.179/24 gw4 192.168.1.1 ipv4.method manual
+nmcli conn add type bridge-slave autoconnect yes con-name enp0s3 ifname enp0s3 master kvmbr0
+
+reboot
