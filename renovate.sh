cad1ab6d6e53f2b43ad072a8e5daff35230942cd
蔡孟儒 <ppksgopin@gmail.com>
2024-07-15 10:31:07 +0800

    Update renovate.sh

--- renovate.sh
+++ renovate.sh
@@ -1,18 +1,15 @@
 #! /bin/bash
 
-sudo dnf install {kernel-headers,kernel-devel} -y
-sudo dnf update -y
+dnf install {kernel-headers,kernel-devel} -y
+dnf update -y
 
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y yum-utils
+dnf install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker && sudo systemctl enable --now docker
+
+dnf install ntfs-3g -y
 
 sudo dnf install epel-release -y
 sudo dnf install https://download1.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm -y
 sudo yum install vlc -y
-
-sudo dnf install ntfs-3g -y
-
-
-sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
-sudo dnf install pass -y
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker
-sudo dnf install docker-ce docker-ce-cli containerd.io -y
-sudo systemctl start docker && sudo systemctl enable --now docker
