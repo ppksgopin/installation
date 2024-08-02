b30e4efcffd0ddf4c1eb70f184b3c6b40e291c84
蔡孟儒 <ppksgopin@gmail.com>
2024-08-02 15:21:45 +0800

    Update jdownloader.sh

--- services/jdownloader.sh
+++ services/jdownloader.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-sudo docker run -d --init --restart=always -v /home/ppksgopin/downloads/jdownloader:/opt/JDownloader/Downloads -v /home/ppksgopin/downloads/jdownloader/config:/opt/JDownloader/app/cfg --name jdownloader -p 3129:3129 -e MYJD_USER=ppksgopin@gmail.com -e MYJD_PASSWORD=Azxpolmppk0215 -e MYJD_DEVICE_NAME=ppksgopin jaymoulin/jdownloader 
+mkdir -p /home/ppksgopin/Downloads/jdownloader/config
+sudo docker run -d --init --restart=always -v /home/ppksgopin/Downloads/jdownloader:/opt/JDownloader/Downloads -v /home/ppksgopin/Downloads/jdownloader/config:/opt/JDownloader/app/cfg --name jdownloader -p 3129:3129 -e MYJD_USER=ppksgopin@gmail.com -e MYJD_PASSWORD=Azxpolmppk0215 -e MYJD_DEVICE_NAME=ppksgopin jaymoulin/jdownloader 
